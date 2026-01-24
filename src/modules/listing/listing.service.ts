4cb8cde026f5ff1a832cd43384ca39c4d676ea49
tundenewcodes <fasasipelumi1652@gmail.com>
2026-01-24 23:59:24 +0100

    db added

--- src/modules/listing/listing.service.ts
+++ src/modules/listing/listing.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { CreateListingDto } from "./dto/create-listing.dto";
+import { PrismaService } from "src/database/prisma.service";
 
 @Injectable()
 export class ListingService {
+  constructor(private readonly prismaService: PrismaService) {}
   create(createListingDto: CreateListingDto) {
     return {
       message: "This action adds a new listing",
